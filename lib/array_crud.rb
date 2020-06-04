419295e5ba953b2eee0bc6ca5fffd7674a1a0e4c
Sara Amiry <saraamiry24@gmail.com>
2020-06-04 22:41:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,16 +14,8 @@ def add_element_to_start_of_array(array, element)
   array.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
-  array.pop
-end
-
-def remove_element_from_start_of_array(array)
-  array.shift
-end
-
 def retrieve_element_from_index(array, index_number)
-  array.index(array, "am")
+  array.index("am")
 end
 
 def retrieve_first_element_from_array(array)
@@ -32,4 +24,12 @@ end
 
 def retrieve_last_element_from_array(array)
   array.index(array)
+end
+
+def remove_element_from_end_of_array(array)
+  array.pop
+end
+
+def remove_element_from_start_of_array(array)
+  array.shift
 end
