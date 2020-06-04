650d650e3b15ee02cec4d87534b95102339f29e4
Sara Amiry <saraamiry24@gmail.com>
2020-06-04 22:23:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  myArray.push(myArray, "Beauty and the Beast")
+  myArray.push(array, "Beauty and the Beast")
 end
 
 def add_element_to_start_of_array(array, element)
