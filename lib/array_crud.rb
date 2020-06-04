ad36b7547bfbc6b8093b6cbbd3efff3a575ad826
Sara Amiry <saraamiry24@gmail.com>
2020-06-04 22:21:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  myArray = Array.new
 end
 
 def create_an_array
-  
+  myArray = ["Mamma Mia", "Small Foot", "Big Foot", "Toy Story"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  myArray.push ("Beauty and the Beast")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  myArray.unshift "Harry Potter"
 end
 
 def remove_element_from_end_of_array(array)
